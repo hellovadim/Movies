f127219e3b6003d000b2aff1639bc791c8000a90
Vadim <69042411+hellovadim@users.noreply.github.com>
2021-04-22 19:53:41 +0300

    Начал новый проект Movies с использованием React Materialize OMDb Api по поиску фильмов

---
+++ src/layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ function Header() {
+    return (
+        <nav className="teal darken-3">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="sass.html">Repo</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+export {Header};
